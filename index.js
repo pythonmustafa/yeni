13a0c46dfcb8995e68d66ed5feb0fe9a62cca909
pythonmustafa <115861627+pythonmustafa@users.noreply.github.com>
2026-06-03 14:39:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/deneme", (req, res) => {
+  res.sendFile("deneme sayfası");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
