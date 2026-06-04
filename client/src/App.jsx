dd4b2021b405580d6aa66fa3cc4360e67563368f
pythonmustafa <115861627+pythonmustafa@users.noreply.github.com>
2026-06-04 06:57:37 +0000

    Sayfa ekleme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
+// 1. Yeni sayfamızı içe aktarıyoruz
+import CariYonetimi from './pages/CariYonetimi'; 
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        
+        {/* 2. Yeni sayfamızın rotasını (URL yolunu) belirliyoruz */}
+        <Route path="/cari" element={<CariYonetimi />} /> 
+        
       </Routes>
     </BrowserRouter>
   );
