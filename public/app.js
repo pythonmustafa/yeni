1d71d139d2de8467d0908a74bc49d77d4d4da307
pythonmustafa <115861627+pythonmustafa@users.noreply.github.com>
2026-06-04 05:38:39 +0000

    gereksiz dosyaları sil

--- public/app.js
+++
@@ -1,8 +0,0 @@
-function getUsers() {
-  fetch("/api/users")
-    .then(res => res.json())
-    .then(data => {
-      document.getElementById("output").innerText =
-        JSON.stringify(data, null, 2);
-    });
-}
